3de6f4b2d5eedacdb982e472288efc04856f07ee
pamarthich <krishnapamarthi@Krishnas-MacBook-Air.local>
2021-11-19 01:37:24 +0530

    added build stages

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,28 @@ ls -lth
 kubectl	 get po 
 eval $(minikube docker-env)
 docker images
+
+# create workdir
+mkdir ~/src
+cd ~/src
+
+# Cloning the api-repositories
+git clone git@github.com:tricolorsoft/tutorsapp-api.git
+
+# changing to develop branch
+cd tutorsapp-api
+git checkout -b develop origin/develop
+
+#cloning the Docker files
+cd ~/src
+git clone git@github.com:pamarthich/tricolor-dockerfiles.git
+
+#evaluating minikube docker env
+eval $(minikube docker-env)
+
+#Bulding api-uma code and docker Image
+cd ~/src/tricolor-dockerfiles
+cat Dockerfile-api-uma >> Dockerfile
+docker build -t api-uma:v1 .
+
+
